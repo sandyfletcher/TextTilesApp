a0de5b6781a02f84d03dff84056c3ebdcc0c741c
sandyfletcher <patrick.a.r.fletcher@gmail.com>
2025-10-30 17:50:49 -0400

    0.0.4fix

--- metro.config.js
+++ metro.config.js
@@ -2,9 +2,16 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Disable asset hashing to preserve original paths
 config.transformer = {
   ...config.transformer,
-  assetPrefix: '/',
+  assetPlugins: [],
+};
+
+// Keep original asset paths
+config.resolver = {
+  ...config.resolver,
+  assetExts: [...config.resolver.assetExts, 'ttf', 'otf'],
 };
 
 module.exports = config;
